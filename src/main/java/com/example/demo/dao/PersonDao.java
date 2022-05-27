ed1df478f8a565a2503229454fd1b4700c042bfc
Ernest Zottor <ernestzottor@gmail.com>
2022-05-27 11:08:00 +0000

    Initial commit

---
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -0,0 +1,25 @@
+package com.example.demo.dao;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.example.demo.model.Person;
+
+public interface PersonDao {
+
+	public int insertPerson(UUID id, Person person);
+	
+	default int insertPerson(Person person) {
+		UUID id = UUID.randomUUID();
+		return insertPerson(id, person);
+	}
+	
+	public List<Person> selectAllPerson();
+	
+	Optional<Person> selectPersonById(UUID id);
+	
+	public int deletePersonById(UUID id);
+	
+	public int  updatePersonById(UUID id, Person person);
+}
